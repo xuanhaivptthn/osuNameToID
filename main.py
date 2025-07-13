52c4bc5c1504f1c0726f5da6d7d45c139dc09822
Xuan Hai <46238512+xuanhaivptthn@users.noreply.github.com>
2025-07-13 14:51:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 from ossapi import Ossapi, UserLookupKey
 import configparser, os, sys
 
-config = configparser.ConfigParser()
 #read from config
+config = configparser.ConfigParser()
 config.read('config.txt')
-client_id = config['API']['client_id']
-client_secret = config['API']['client_secret']
-if client_id == '' :
+client_id = config['oAPI']['client_id']
+client_secret = config['oAPI']['client_secret']
+if client_id == '' or client_secret == '' :
     sys.exit("Missing API") #sys library
 
 api = Ossapi(client_id, client_secret)
+mainMode = 'osu' # osu, mania, taiko, fruits
 
-#open list
-list = open("list.txt", "r")
+# print(api.user(13456818).statistics.global_rank) # get global rank
+# print(api.user(13456818).statistics.country_rank) # get global rank
+# print(api.user(13456818).country_code) # get country code
+# print(api.user(13456818).id) # get id
+# print(api.user(13456818).username) # get username
+
+#open and import file as lists
+list = open("list.txt", "r").readlines()
 output = open("output.txt", "w")
 print("Processing...")
+print(list)
+
 #process
+count=len(list)
 for X in list:
     X = X.strip('\n') #remove newline character
-    user = api.user(X)
-    print(user.id, file=output)
+    try:
+        user = api.user(X, mode=mainMode)
+        print(user.statistics.global_rank, file=output) # change here for file output
+        count = count - 1
+        print("Get User", user.username, "|", count , "user left.")
+    except Exception as e:
+        print("!!! SKIPPED !!!", X)
+        print("Error: ", e)
+        print("", file=output)
 
 output.close()
 print("Done. Check output.txt")
